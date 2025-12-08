9127d621d4b68f12a741d8e97a8db25265b35854
Alif-Hossen <naimuralif1.0@gmail.com>
2025-12-08 13:32:12 -0600

    Server Site Setup Complete

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Smart Server Is Running");
+})
+
+app.listen(port, () => {
+    console.log(`Smart Server Is Running On Port : ${port}`);
+})
+
+
+
