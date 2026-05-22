964f0e6881a06e39d92fe49e232486d7c7487e3e
Alif-Hossen <naimuralif1.0@gmail.com>
2026-05-22 16:45:00 +0300

    Save Tutor Data In The Database

--- index.js
+++ index.js
@@ -33,41 +33,92 @@ async function run() {
 
     const db = client.db("eTuitionBD");
     const tuitionCollection = db.collection("tuitions");
+    const userCollection = db.collection("users");
+    const tutorCollection = db.collection("tutors");
 
-    // TUITION'S API -->
+    // ---------------------------------------.!
+    // USER'S API ----------------------------->
+    // ---------------------------------------.!
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      user.createdAt = new Date();
 
+      const email = user.email;
+      const userExist = await userCollection.findOne({ email });
+
+      if (userExist) {
+        return res.send({ message: "User Already Exist" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+
+
+    // ---------------------------------------.!
+    // TUITION'S API -------------------------->
+    // ---------------------------------------.!
     app.post('/tuitions', async (req, res) => {
-        const tuition = req.body;
-        tuition.createdAt = new Date();
-        const result = await tuitionCollection.insertOne(tuition);
-        res.send(result);
+      const tuition = req.body;
+      tuition.createdAt = new Date();
+      const result = await tuitionCollection.insertOne(tuition);
+      res.send(result);
     })
 
     app.get('/myTuitions', async (req, res) => {
-        const query = {};
-        
-        const {email} = req.query;
+      const query = {};
 
-        // TUITIONS?EMAIL=' ' &  -->
-        if(email){
-            query.email = email;
-        }
+      const { email } = req.query;
 
-        const options = {
-            sort: { createdAt: -1 }
-        }
+      // TUITIONS?EMAIL=' ' &  -->
+      if (email) {
+        query.email = email;
+      }
 
-        const cursor = tuitionCollection.find(query, options);
-        const result = await cursor.toArray();
-        res.send(result);
+      const options = {
+        sort: { createdAt: -1 }
+      }
+
+      const cursor = tuitionCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     app.delete('/myTuitions/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        
-        const result = await tuitionCollection.deleteOne(query);
-        res.send(result);
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await tuitionCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // ---------------------------------------.!
+    // TUTOR'S API -------------------------->
+    // ---------------------------------------.!
+
+    app.get('/tutors', async (req, res) => {
+      const query = {};
+
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+
+      const cursor = tutorCollection.find(query);
+
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/tutors', async (req, res) => {
+      const tutor = req.body;
+      tutor.status = "pending";
+      tutor.createdAt = new Date();
+
+      const result = await tutorCollection.insertOne(tutor);
+      res.send(result);
     })
 
 
@@ -84,11 +135,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Smart Server Is Running");
+  res.send("Smart Server Is Running");
 })
 
 app.listen(port, () => {
-    console.log(`Smart Server Is Running On Port : ${port}`);
+  console.log(`Smart Server Is Running On Port : ${port}`);
 })
 
 
